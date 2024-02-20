dcab8cbf866fc9604a440f3bcb561ccf9e32cc95
leeseungjun <cjstkrak@gmail.com>
2024-02-20 18:35:17 +0900

    fix: 둘만의암호 해결

--- Programmers/lv1/둘_만의_암호.js
+++ Programmers/lv1/둘_만의_암호.js
@@ -1,26 +1,29 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/155652
 
 function solution(s, skip, index) {
-  var answer = "";
-  const alphabat = "abcdefghijklmnopqrstuvwxyz";
+  let answer = "";
+  let alphabat = "abcdefghijklmnopqrstuvwxyz";
+  const skipStr = [...skip];
+  for (skip of skipStr) {
+    alphabat = alphabat.replace(skip, "");
+  }
 
-  const before = [...s];
-  const skipArr = [...skip];
-
-  const skipindex = skipArr.map((i) => {
-    let beforeIndex = alphabat.indexOf(i);
-  });
-  before.forEach((i) => {
-    let beforeIndex = alphabat.indexOf(i);
-    let newIndex = beforeIndex + index;
-
-    if (newIndex > 25) newIndex = newIndex - 26;
-
-    answer += alphabat[newIndex];
-  });
+  let sArr = [...s];
+  for (let i = 0; i < sArr.length; i++) {
+    let beforeIndex = alphabat.indexOf(sArr[i]);
+    let afterIndex = beforeIndex + index;
+    while (afterIndex >= alphabat.length) {
+      afterIndex -= alphabat.length;
+    }
+    answer += alphabat[afterIndex];
+  }
 
   console.log(answer);
   return answer;
 }
 
-solution("aukks", "asasas", 7);
+solution("aukks", "wbqd", 5);
+
+// 스킵이 10개 인덱스가 20개라고 치자
+// 26개 - 10 = 16개 여기서 20개면
+// z
