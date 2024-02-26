4994747a2dfe68d6bfbe0c062b403f84c81c65e9
leeseungjun <cjstkrak@gmail.com>
2024-02-26 13:51:55 +0900

    add: 시소짝궁

---
+++ Programmers/lv2/시소_짝궁.js
@@ -0,0 +1,13 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/152996
+
+function solution(weights) {
+  var answer = 0;
+  let current;
+  for (let i = 0; i < weights; i++) {}
+
+  return answer;
+}
+
+// weight = [100,180,360,100,270]
+// 몸무게가 같으면 추가 나의 배수가 있으면 추가
+// 내 몸무게의 배수가 있으면 뺀다
