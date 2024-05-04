f06d7cb969b404b3bfbe163628a00a49876e1402
leeseungjun <cjstkrak@gmail.com>
2024-05-04 13:27:02 +0900

    add: 스택 lv2올바른괄호

--- Programmers/lv2/올바른_괄호.js
+++ Programmers/lv2/올바른_괄호.js
@@ -1,38 +1,21 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12909
-
-// function solution(s) {
-//   const newArr = s.split("");
-//   let shit = 0;
-
-//   while (newArr.length !== 0) {
-//     let left = newArr.shift();
-//     if (shit >= 0) {
-//       if (left == "(") shit++;
-//       else shit--;
-//     } else {
-//       return false;
-//     }
-//   }
-
-//   return shit === 0 ? true : false;
-// }
-
-// console.log(solution("()()())"));
-// 왼쪽의 개수랑 오른쪽의 개수랑
-
 function solution(s) {
   const stack = [];
 
-  for (const char of s) {
-    if (char === "(") {
-      stack.push(char);
-    } else if (char === ")") {
-      if (stack.length === 0 || stack.pop() !== "(") {
-        return false;
-      }
+  for (let i of s) {
+    if (i === "(") {
+      // '(' 라면 ')' 를 push
+      stack.push(")");
+    } else {
+      // ')' 라면
+      if (stack.length === 0 || stack.pop() !== ")") return false;
     }
   }
 
-  return stack.length === 0;
+  return stack.length === 0 ? true : false;
 }
-console.log(solution("()()())"));
+
+let a = solution("((()()))");
+console.log(a);
+// 1. stack 구조 이용 LIFO
+// 2. O(n)
